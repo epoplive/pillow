0f58d2ff75f4215063f722ad1df65d3af4f4da18
Brett Thomas <bthomas@cloudflare.com>
2015-06-05 21:07:54 -0700

    test

---
+++ tests/FrontControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * FrontControllerTest.php
+ * Author: Brett Thomas <brett.thomas@gmail.com>
+ */
+
+namespace pillow\tests;
+
+
+class FrontControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function definedPillowRootPath()
+    {
+
+    }
+}
